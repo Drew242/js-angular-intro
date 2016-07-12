c906c2b843efbe07ead97abeedb0aba4c5c5169b
Drew Conly <atconly@gmail.com>
2016-07-12 10:55:53 -0600

    ng-repeat

--- main.js
+++ main.js
@@ -1,48 +1,63 @@
-// First component needed - Module.
-// Basically a 'bucket' for code (container for OTHER angular elements)
+angular.module('PokeApp', []);
 
-//create module (takes one or two args - name & an empty array, which is an injector)
-angular.module('helloModule', [])
-//dependency injection
+angular.module('PokeApp')
+  .controller('PallettTown', [pallettCtrl]);
 
-// Next big component to Angular is CONTROLLER
-// Probably most common angular component
-// Allows you to expose data to the view - glue between Model & view
+  function pallettCtrl() {
+    var pCtrl = this;
 
-// with just the name argument, this retrieves the module
-angular.module('helloModule')
-  // the final method in the injector MUST be a function that represent the controller itself
-  .controller('alpha', [
-      '$scope', // this is the object that will expose data to the view
-      alphaCtrl
-  ]);
-
-angular.module('helloModule')
-  // the final method in the injector MUST be a function that represent the controller itself
-  .controller('betaCtrl', betaCtrl); // this is the object that will expose data to the view);
-
-  // This is the funciton that represents the controller
-  // Must pass, as parameters, the dependencies injected above
-  function alphaCtrl($scope){
-    $scope.greeting = "Welcome to the Command Center!";
-    $scope.subGreeting = "Power Rangers ONLY";
-    $scope.fightPutty = function() {
-      $scope.subGreeting = 'punch noises';
-    }
-  }
-
-  function betaCtrl(){
-    var beta        = this;
-    beta.beans      = 'Black Beans';
-
-    beta.myBeans    = [];
-
-    beta.addBeans = function(myFavoriteBeans){
-      beta.myBeans.push(beta.myFavoriteBeans);
-    }
-    beta.removeBean = function() {
-      beta.myBeans.pop();
-    }
-    window.beta = beta;
-    // this is for debugging and allows you to type Angular into the console
+    pCtrl.welcomeMessage = "Gotta Catch 'Em All";
+    pCtrl.pokemon        = [
+          'Charmander',
+          'Bulbasaur',
+          'Squirtle',
+          'Growlithe',
+          'Mankey',
+          'Cubone',
+          'Dugtrio',
+          'Pidgeotto',
+          'Pikachu'
+    ];
+    pCtrl.clonedPokemon = [
+          'Pikachu',
+          'Pikachu',
+          'Pikachu',
+          'Pikachu',
+          'Pikachu',
+          'Pikachu',
+          'Rhydon'
+    ];
+    pCtrl.trainers = [
+      {
+        name : 'Ash',
+        badges : [
+          'Earth',
+          'Wind',
+          'Fire'
+        ],
+        skill : 1,
+      },
+      {
+        name : 'Misty',
+        badges : [
+          'Water',
+        ],
+        skill : 3,
+      },
+      {
+        name : 'Brock',
+        badges : [
+          'Rock',
+          'Suplex',
+          'Cascade'
+        ],
+        skill : 5,
+      }
+    ];
+    pCtrl.jigglypuff = {
+      squishiness : 7,
+      rage        : 3,
+      rockstar    : 'sure...',
+      name        : 'jigglypugg'
+    };
   }
