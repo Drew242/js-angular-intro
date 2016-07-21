ec5e2c6bc71909d0067ef3701bb7d2478c172ec0
Drew Conly <atconly@gmail.com>
2016-07-21 13:16:55 -0600

    chuck norris

--- main.js
+++ main.js
@@ -1,15 +1,41 @@
 angular.module('Chuckuthon', []);
 
-angular.module('ChuckFactory')
+angular.module('Chuckuthon')
+  .controller('ChuckingAwesomeController', ChuckingAwesome)
   .factory('ChuckFactory', ChuckFactory)
 
-function ChuckFactory() {
+ChuckingAwesome.$inject = ['ChuckFactory'];
+ChuckFactory.$inject = ['$http'];
+
+function ChuckingAwesome(ChuckFactory) {
+  var chuck = this;
+  chuck.title = 'Time waits for no man. Unless than man is Chuck Norris';
+  chuck.quote = '';
+
+  chuck.newJoke = function() {
+    ChuckFactory.getRandomQuote()
+    .then(function(res){
+      var data = res.data || {}
+      value = data.value || {}
+      joke = value.joke || ''
+
+      chuck.quote = joke || ':( no joke found';
+    }, function(error) {
+      console.error(error);
+      try {
+        // unsafe code //
+      } catch(error) {
+        console.error(error); 
+      }
+    })
+  }
 
 }
 
-angular.module('Chuckuthon')
-  .controller('ChuckingAwesomeController', ChuckingAwesomeController)
-
-function ChuckingAwesomeController() {
-  var cAc = this; 
+function ChuckFactory($http) {
+  return {
+    getRandomQuote: function() {
+      return $http.get('https://api.icndb.com/jokes/random');
+    }
+  }
 }
